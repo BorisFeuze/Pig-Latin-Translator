033b8e4cf9e129a32bdf40b333021fe6ef54d44c
Boris Feuze <boris.feuze1980@gmail.com>
2025-08-01 16:51:31 +0200

    new code

--- pig-latin-translator.js
+++ pig-latin-translator.js
@@ -1,24 +1,16 @@
 const args = process.argv.slice(2);
 
-if (args.length < 2) {
-  console.error("Please provide two words to be able to play");
-  // return;
-  process.exit(1);
-}
+// if (args.length < 2) {
+//   console.error("Please provide two words to be able to play");
+//   // return;
+//   process.exit(1);
+// }
 
 let angabe1 = args[0].toLowerCase().trim();
 let angabe2 = args[1].toLowerCase().trim();
 
-console.log(angabe1);
-console.log(angabe2);
-
-const arrayAngabe = Array.from(angabe1);
-// console.log(arrayAngabe);
-
-let firstLetterAngabe = [];
-let secondLetterAngabe = [];
-
-let result = [];
+// console.log(angabe1);
+// console.log(angabe2);
 
 const consonantLetter = [
   "b",
@@ -46,31 +38,71 @@ const consonantLetter = [
 
 const vowelLetter = ["a", "e", "i", "o", "u"];
 
-if (consonantLetter.some((letter) => letter === arrayAngabe[0])) {
-  firstLetterAngabe = arrayAngabe.shift();
-  // console.log(arrayAngabe);
-
-  if (consonantLetter.some((letter) => letter === arrayAngabe[0])) {
-    secondLetterAngabe = arrayAngabe.shift();
-    // console.log(arrayAngabe);
-  }
-} else if (vowelLetter.some((letter) => letter === arrayAngabe[0])) {
-}
-
-if (firstLetterAngabe) {
-  for (let i = 0; i < firstLetterAngabe.length; i++) {
-    arrayAngabe.push(firstLetterAngabe[i]);
-  }
-
-  for (let i = 0; i < secondLetterAngabe.length; i++) {
-    arrayAngabe.push(secondLetterAngabe[i]);
-  }
-}
-
+const arrayAngabe = angabe1.split(" ");
 // console.log(arrayAngabe);
 
-result = arrayAngabe.join("") + angabe2;
+let result = [];
 
-console.log(
-  `Your first word is ${angabe1} and your second word is ${angabe2}. Both words togehter built ${result}`
-);
+let temporalyResult = [];
+// let temporalyResult2 = [];
+
+for (let i = 0; i < arrayAngabe.length; i++) {
+  let word = arrayAngabe[i];
+
+  let arrayWord = Array.from(word);
+
+  let firstLetterWord = [];
+  let secondLetterWord = [];
+
+  console.log(arrayWord);
+
+  for (let i = 0; i < arrayWord.length; i++) {
+    let isConsonantLetter = false;
+    let isVowelLetter = false;
+
+    vowelLetter.some((letter) => {
+      return console.log(letter === arrayWord[0], arrayWord[0]);
+    })
+      ? (isVowelLetter = true)
+      : (isVowelLetter = false);
+    consonantLetter.some((letter) => letter === arrayWord[0])
+      ? (isConsonantLetter = true)
+      : (isConsonantLetter = false);
+
+    console.log(isVowelLetter);
+    console.log(isVowelLetter);
+
+    if ((isVowelLetter = false)) {
+      temporalyResult.push(arrayWord.slice().join("") + angabe2);
+      console.log(temporalyResult);
+    } else if (isConsonantLetter) {
+      firstLetterWord = arrayWord.shift();
+      // console.log(arrayWord);
+
+      if ((isConsonantLetter = false)) {
+        secondLetterWord = arrayWord.shift();
+      }
+
+      if (firstLetterWord) {
+        for (let i = 0; i < firstLetterWord.length; i++) {
+          arrayWord.push(firstLetterWord[i]);
+        }
+
+        for (let i = 0; i < secondLetterWord.length; i++) {
+          arrayWord.push(secondLetterWord[i]);
+        }
+      }
+      console.log(arrayWord);
+      let x = arrayWord.join("");
+
+      console.log(x);
+      temporalyResult.push(arrayWord.join("") + angabe2);
+      console.log(temporalyResult);
+      // console.log(temporalyResult1);
+    }
+  }
+}
+
+result = temporalyResult.join(" ");
+
+console.log(result);
